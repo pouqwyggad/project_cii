36e842339a6c0d600fe673fbcae3e307dded8a57
pouqwyggad <sultanusmanov176@gmail.com>
2023-06-08 20:11:59 +0300

    encoded

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn import preprocessing
+from sklearn.preprocessing import LabelEncoder
 
-data = pd.read_csv('/Users/smhn/Projects/project_cii/android_apps_traffic_attributes_prepared.csv')
+data = pd.read_csv('android_apps_traffic_attributes_prepared.csv')
 
 miss_values = data.isnull().sum()
 print("Проверка пропущенных значений по столбцам:")
 print(miss_values)
-
 
 data = data.dropna()  # удаление строк с пропущенными значениями
 
@@ -14,19 +15,15 @@ duplicates = data.duplicated()
 print("Проверка наличия дубликатов:")
 print(duplicates.sum())
 
-
-data = data.drop_duplicates() # удаляем дубликаты, если они есть
-
+data = data.drop_duplicates()  # удаляем дубликаты, если они есть
 
 data_types = data.dtypes
 print("Анализ типов данных:")
 print(data_types)
 
-
 numeric_features = data.select_dtypes(include=['float64', 'int64'])
 print("Определение числовых признаков:")
 print(numeric_features.columns)
-
 
 categorical_features = data.select_dtypes(include=['object'])
 print("Определение категориальных признаков:")
@@ -34,12 +31,8 @@ print(categorical_features.columns)
 
 encoded = pd.get_dummies(data, columns=categorical_features.columns)
 
-
 numeric_columns = numeric_features.columns
 outliers = pd.DataFrame(columns=numeric_columns)
-
-
-
 
 # Проверка наличия выбросов в каждом числовом признаке
 for column in numeric_columns:
@@ -60,7 +53,6 @@ for column in numeric_columns:
 print("Выбросы в числовых признаках:")
 print(outliers)
 
-
 # Создание диаграммы размаха для каждого числового признака
 for column in numeric_columns:
     plt.figure(figsize=(6, 4))
@@ -71,4 +63,16 @@ for column in numeric_columns:
     # plt.savefig(column + ".png")
     plt.show()
 
-    # мотя лох
+
+# фильтрация столца app_encryption
+data = data[data["app_encryption"] == "yes"]
+
+# удаляем ненужные атрибуты
+excludedAttributes = ['L3_Src_Addr', 'L3_Dst_Addr', 'flow_id', 'app_encryption']
+data = data.drop(excludedAttributes, axis=1)
+
+# кодируем целевой признак "app_ip" при помощи LabelEncoder
+le = preprocessing.LabelEncoder()
+data['encoded'] = le.fit_transform(data['app_id'])
+
+data.to_csv('updated_android_apps_traffic_attributes_prepared.csv', index=False)
